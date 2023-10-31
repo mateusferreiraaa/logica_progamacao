a043174493571360032d0ea903cfbb0784fd9e7c
Mateus <mateusfm04@gmail.com>
2023-10-31 15:48:10 -0300

    Questão 14 e 15

---
+++ q 15.py
@@ -0,0 +1,3 @@
+# eu encontrei que ao utilizarmos as aspas triplas o Python entende as aspas simples e duplas são parte da string e não um marcador especial
+frase1='''Ele perguntou, "Vocês falaram a verdade para a policia?"'''
+print(frase1)
